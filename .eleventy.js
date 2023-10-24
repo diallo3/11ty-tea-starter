ea7481b218838b576c3790ca7bd27395980c5ff9
Diallo Hudgins <d@diallohudgins.com>
2023-10-24 19:49:23 -0400

    Initial commit
    Signed-off-by: Diallo Hudgins <d@diallohudgins.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,97 +1,97 @@
-const path = require('path')
+const path = require("path");
 const pluginRss = require("@11ty/eleventy-plugin-rss"); // needed for absoluteUrl SEO feature
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 const Image = require("@11ty/eleventy-img");
 const yaml = require("js-yaml"); // Because yaml is nicer than json for editors
-require('dotenv').config();
+require("dotenv").config();
 
 const baseUrl = process.env.BASE_URL || "http://localhost:8080";
-console.log('baseUrl is set to ...', baseUrl);
+console.log("baseUrl is set to ...", baseUrl);
 
 const globalSiteData = {
-  title: "11ty Starter Site",
-  description: "This is a basic 11ty starter template with my most commonly used features and modern tooling",
-  locale: 'en',
-  baseUrl: baseUrl,
-}
+    title: "11ty Starter Site",
+    description:
+        "This is a basic 11ty starter template with my most commonly used features and modern tooling",
+    locale: "en",
+    baseUrl: baseUrl,
+};
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+    /* --- GLOBAL DATA --- */
 
-  /* --- GLOBAL DATA --- */
-  
-  eleventyConfig.addGlobalData("site", globalSiteData);
+    eleventyConfig.addGlobalData("site", globalSiteData);
 
-  /* --- YAML SUPPORT --- */
-  
-  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
-  eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
+    /* --- YAML SUPPORT --- */
 
-  /* --- PASSTHROUGHS --- */
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+    eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
 
-  eleventyConfig.addPassthroughCopy('src/assets/css')
-	eleventyConfig.addPassthroughCopy('src/assets/js')
+    /* --- PASSTHROUGHS --- */
 
+    eleventyConfig.addPassthroughCopy("src/assets/css");
+    eleventyConfig.addPassthroughCopy("src/assets/js");
 
-  /* --- PLUGINS --- */
+    /* --- PLUGINS --- */
 
-  eleventyConfig.addPlugin(pluginRss); // just includes absolute url helper function
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPlugin(EleventyVitePlugin, {});
+    eleventyConfig.addPlugin(pluginRss); // just includes absolute url helper function
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(EleventyVitePlugin, {});
 
-  /* --- SHORTCODES --- */
+    /* --- SHORTCODES --- */
 
-  // Image shortcode config
-  let defaultSizesConfig = "(min-width: 1200px) 1400px, 100vw"; // above 1200px use a 1400px image at least, below just use 100vw sized image
+    // Image shortcode config
+    let defaultSizesConfig = "(min-width: 1200px) 1400px, 100vw"; // above 1200px use a 1400px image at least, below just use 100vw sized image
 
-  eleventyConfig.addShortcode("image", async function(src, alt, sizes=defaultSizesConfig) {
-		console.log(`Generating image(s) from:  ${src}`)
-    let metadata = await Image(src, {
-			widths: [800, 1500],
-			formats: ["webp", "jpeg"],
-      urlPath: "/images/",
-			outputDir: "./_site/images/",
-			filenameFormat: function (id, src, width, format, options) {
-				const extension = path.extname(src)
-				const name = path.basename(src, extension)
-				return `${name}-${width}w.${format}`
-			}
-		});
+    eleventyConfig.addShortcode(
+        "image",
+        async function (src, alt, sizes = defaultSizesConfig) {
+            console.log(`Generating image(s) from:  ${src}`);
+            let metadata = await Image(src, {
+                widths: [800, 1500],
+                formats: ["webp", "jpeg"],
+                urlPath: "/images/",
+                outputDir: "./_site/images/",
+                filenameFormat: function (id, src, width, format, options) {
+                    const extension = path.extname(src);
+                    const name = path.basename(src, extension);
+                    return `${name}-${width}w.${format}`;
+                },
+            });
 
-		let imageAttributes = {
-			alt,
-			sizes,
-			loading: "lazy",
-			decoding: "async",
-		};
+            let imageAttributes = {
+                alt,
+                sizes,
+                loading: "lazy",
+                decoding: "async",
+            };
 
-		return Image.generateHTML(metadata, imageAttributes);
-	});
+            return Image.generateHTML(metadata, imageAttributes);
+        }
+    );
 
-  // Output year for copyright notices
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+    // Output year for copyright notices
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
+    /* --- FILTERS --- */
 
-  /* --- FILTERS --- */
+    // Custom Random Helper Filter (useful for ID attributes)
+    eleventyConfig.addFilter("generateRandomIdString", function (prefix) {
+        return prefix + "-" + Math.floor(Math.random() * 1000000);
+    });
 
-  // Custom Random Helper Filter (useful for ID attributes)
-  eleventyConfig.addFilter("generateRandomIdString", function (prefix) {
-    return prefix + "-" + Math.floor(Math.random() * 1000000);
-  });
+    /* --- BASE CONFIG --- */
 
-
-  /* --- BASE CONFIG --- */
-
-  return {
-    dir: {
-      input: "src",
-      output: "_site",
-      includes: "includes", // this path is releative to input-path (src/)
-      layouts: "layouts", // this path is releative to input-path (src/)
-      data: "data", // this path is releative to input-path (src/)
-    },
-    templateFormats: ["njk", "md"],
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
-  };
+    return {
+        dir: {
+            input: "src",
+            output: "_site",
+            includes: "_includes", // this path is relative to input-path (src/)
+            layouts: "_layouts", // this path is relative to input-path (src/)
+            data: "_data", // this path is relative to input-path (src/)
+        },
+        templateFormats: ["njk", "md"],
+        htmlTemplateEngine: "njk",
+        markdownTemplateEngine: "njk",
+    };
 };
--- src/posts/my-first-post.md
+++
@@ -1,7 +0,0 @@
----
-layout: base
-title: My First Post
-tags: post
----
-
-Hello from {{ title }}!
